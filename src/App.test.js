5b8e8b9b17c9e55ee4528c84f647a811343addfc
Mauricio Collazos <ma0@contraslash.com>
2019-11-25 23:03:08 -0500

    Added bad test

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('A very bad test that fails', () => {
+  expect(false).toBeTruthy();
+});
